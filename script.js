d3fa1f002e03ae4623d1a0d298c252e6032707a3
Ashley <ashleyvport@gmail.com>
2018-04-07 15:59:31 -0400

    js

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-var person = prompt("Please enter your name", "Harry Potter");
+function msg(){  
+ alert("Hello Javatpoint");  
+}  
 
-if (person == null || person == "") {
-    txt = "User cancelled the prompt.";
-} else {
-    txt = "Hello " + person + "! How are you today?";
-} 
+
+
+
+function myFunction() {
+  var y = document.getElementById("txt1").value;
+  var z = document.getElementById("txt2").value;
+  var x = y + z;
+  document.getElementById("demo").innerHTML = x;
+}
+
+
+function myNumber() {
+const myNumber = () => {
+	let i=2;
+	console.log(i);
+	for (let i=0; i<10; i++) {
+		console.log(i);
+	}
+}
+}
+
+var result = '';
+var i = 0;
+do {
+   i += 1;
+   result += i + ' ';
+} while (i < 5);
+document.getElementById('example').innerHTML = result;
